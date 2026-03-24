89e6e218bf6510f14a2e62b346c57be3d0a8c0fa
2997392t <2997392t@student.gla.ac.uk>
2026-03-24 03:49:30 +0000

    test fix 2

--- fergusonbequest/tests/test_attraction_waitlist.py
+++ fergusonbequest/tests/test_attraction_waitlist.py
@@ -258,11 +258,11 @@ class AttractionWaitlistUITests(TestCase):
         self.assertEqual(response.status_code, 200)
         content = response.content.decode("utf-8")
 
-        # Attraction page should still show join UI
-        self.assertIn("Join Waiting List", content)
-
-        # Leave button should not appear here anymore
+        # Should show leave button
         self.assertIn("Leave Waiting List", content)
+        
+        # Should not show join button
+        self.assertNotIn("Join Waiting List", content)
 
 
 class AttractionWaitlistAccessTests(TestCase):
